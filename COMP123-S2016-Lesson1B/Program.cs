45b781e4bff31b4698678bf778f369e89ba99fef
Kiritoinder <inderbharaj@ymail.com>
2016-05-13 11:04:28 -0400

    Added GetUserName method

--- COMP123-S2016-Lesson1B/Program.cs
+++ COMP123-S2016-Lesson1B/Program.cs
@@ -9,6 +9,8 @@ using System.Threading.Tasks;
  * Date: 13 May-2016
  * Student ID: 300874118
  * Description: this is the lesson 1B for github
+ * 
+ * Version: 0.2 - Added GetUserName method
  */
 
 
@@ -20,6 +22,30 @@ namespace COMP123_S2016_Lesson1B
     {
         static void Main(string[] args)
         {
+            GetUserName();
         }
+
+        /**
+         *This method reads user names from the console
+         *
+         * @method GetUserName
+         * @return {string} UserName
+         * */
+
+
+
+        public static string GetUserName()
+        {
+            // initialize Variable
+            string UserName = "";
+            Console.Write("Enter User Name");
+            UserName = Console.ReadLine();
+            
+            Console.WriteLine("/n=========================");
+            Console.WriteLine("You Entered: " + UserName);
+            return UserName;
+        }
+
+
     }
 }
