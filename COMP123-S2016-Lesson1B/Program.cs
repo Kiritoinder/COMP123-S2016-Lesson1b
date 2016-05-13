ee760b57ed5b0fe3e643d7c53c62f685915c5caa
Kiritoinder <inderbharaj@ymail.com>
2016-05-13 10:44:07 -0400

    Initial commit

---
+++ COMP123-S2016-Lesson1B/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ * Author: Inderjeet Singh
+ * Date: 13 May-2016
+ * Student ID: 300874118
+ * Description: this is the lesson 1B for github
+ */
+
+
+
+
+namespace COMP123_S2016_Lesson1B
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
